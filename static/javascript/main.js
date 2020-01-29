a76d85644a8124ce858d9f1547d976719850d6ea
yugo nakamura <yugonakamura@yugonoMacBook-Pro.local>
2020-01-29 12:19:57 +0900

    update layout #4 フッター追加したりレイアウト調整したりメニュー改善したり

--- static/javascript/main.js
+++ static/javascript/main.js
@@ -7,9 +7,27 @@ $(function(){
     let parts = url.split("#");
     let target = parts[1];
 
-    let targetOffset = $("#" + target).offset().top;
+    let targetOffset = $("#" + target).offset().top - 48;
 
     $("html, body").animate({scrollTop: targetOffset}, 750, 'swing');
-
   });
+  $(window).scroll(function(){
+    var scroll = $(window).scrollTop();
+    var switchPos = $(".contents").offset();
+  
+    if(scroll > switchPos.top){
+      $('.menu').addClass('fixed');
+    }else{
+      $('.menu').removeClass('fixed');
+    }
+  });
+
+  $("#stressns").click(function(){
+    const portal = document.querySelector('portal');
+    $('.portal_demo').addClass("active");
+    portal.addEventListener('click', () => portal.activate());
+
+  })
+
 });
+
